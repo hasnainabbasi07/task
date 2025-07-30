c1c0aec74d0cf970b2bf5bcc369ccd80fd6ebd93
hasnainabbasi07 <hasnainhabib565@gmail.com>
2025-07-30 14:24:47 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import HeroSection from './components/HeroSection';
+import FeaturesGrid from './components/FeaturesGrid';
+import HighlightSection from './components/HighlightSection';
+import InstallSteps from './components/InstallSteps';
+import MidCTA from './components/MidCTA';
+import Testimonials from './components/Testimonials';
+import FooterCTA from './components/FooterCTA';
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesGrid />
+      <HighlightSection />
+      <InstallSteps />
+      <MidCTA />
+      <Testimonials />
+      <FooterCTA />
+    </>
+  );
+}
+
+export default App;
